26decc8d07d7e87c5bdee4d3f73a85bf03fe9295
Christian Himpe <c_himp01@DERRIAL.uni-muenster.de>
2013-11-20 14:13:29 +0100

    new demos

---
+++ GRAM.m
@@ -0,0 +1,15 @@
+function W = GRAM(a,b,c,w,varargin)
+% GRAM (shortcut encapsulation)
+% by Christian Himpe, 2013 ( http://gramian.de )
+% released under BSD 2-Clause License ( opensource.org/licenses/BSD-2-Clause )
+%*
+
+if(exist('emgr')~=2) disp('emgr framework is required. Download at http://gramian.de/emgr.m'); return; end
+
+J = size(b,2);
+N = size(a,1);
+O = size(c,1);
+
+if( (N~=size(a,2)) || (N~=size(b,1)) || (N~=size(c,2)) ), error('ERROR: matrix dimension mismatch'); end;
+
+W = emgr(@(x,u,p) a*x+b*u,@(x,u,p) c*x,[J N O],[0 (1.0/N) 1.0],w,varargin);
---
+++ mobile.m
@@ -0,0 +1,60 @@
+function mobile
+% mobile (linear state reduction)
+% by Christian Himpe, 2013 ( http://gramian.de )
+% released under BSD 2-Clause License ( opensource.org/licenses/BSD-2-Clause )
+%*
+
+if(exist('emgr')~=2) disp('emgr framework is required. Download at http://gramian.de/emgr.m'); return; end
+
+%%%%%%%% Setup %%%%%%%%
+
+ J = 4;
+ N = 16;
+ O = J;
+ R = O;
+ T = [0 0.01 1];
+ L = (T(3)-T(1))/T(2);
+ U = [ones(J,1) zeros(J,L-1)];
+ X =  zeros(N,1);
+
+ rand('seed',1009);
+ A = rand(N,N); A(1:N+1:end) = -0.55*N; A = 0.5*(A+A');
+ B = rand(N,J);
+ C = B';
+
+ LIN = @(x,u,p) A*tanh(x) + B*u;
+ OUT = @(x,u,p) C*x;
+
+%%%%%%%% Reduction %%%%%%%%%
+
+% FULL
+ tic; Y = rk2(LIN,OUT,T,X,U,0); FULL = toc
+
+% OFFLINE
+ tic;
+ WX = emgr(LIN,OUT,[J N O],T,'x');
+ [UU D VV] = svd(WX); UU = UU(:,1:R); VV = VV(:,1:R)';
+ a = VV*A*UU;
+ b = VV*B;
+ c = C*UU;
+ x = VV*X;
+ lin = @(x,u,p) a*x + b*u;
+ out = @(x,u,p) c*x;
+ OFFLINE = toc
+
+% ONLINE
+ tic; y = rk2(lin,out,T,x,U,0); ONLINE = toc
+ ERROR = norm(norm(Y - y)./norm(Y))
+
+%%%%%%%% Integrator %%%%%%%%
+
+function y = rk2(f,g,t,x,u,p)
+
+ h = t(2);
+ T = (t(3)-t(1))/h;
+ y = zeros(numel(g(x,u(:,1),p)),T);
+
+ for t=1:T
+  x = x + h*f(x + 0.5*h*f(x,u(:,t),p),u(:,t),p); %Improved Eulers Method
+  y(:,t) = g(x,u(:,t),p);
+ end
