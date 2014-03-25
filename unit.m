9511a9151fc11b98e40f78573e2e4eafc23fe722
Christian Himpe <c_himp01@ODDMAN.uni-muenster.de>
2014-03-25 15:51:59 +0100

    release candidate

---
+++ unit.m
@@ -0,0 +1,22 @@
+% unit
+% by Christian Himpe, 2013-2014 ( http://gramian.de )
+% released under BSD 2-Clause License ( opensource.org/licenses/BSD-2-Clause )
+%*
+
+disp('testemgr:'); testemgr
+
+disp('state_wx:'); state_wx
+disp('state_wy:'); state_wy
+disp('state_bt:'); state_bt
+disp('hierarchy:'); hierarchy
+disp('param_ws:'); param_ws
+disp('param_wi:'); param_wi
+disp('combined_wj:'); combined_wj
+disp('benchmark_ilp:'); benchmark_ilp
+disp('benchmark_lin:'); benchmark_lin
+disp('benchmark_non:'); benchmark_non
+disp('decentral:'); decentral
+disp('measure:'); measure
+disp('advection:'); advection
+disp('nbody:'); nbody
+disp('blackhole:'); blackhole
