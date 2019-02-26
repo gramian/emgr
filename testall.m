b4223a10b126a5e25d37cb23d11d04a5754ca74c
Christian <himpe@mpi-magdeburg.mpg.de>
2019-02-26 14:54:25 +0100

    emgr v5.7

--- testall.m
+++ testall.m
@@ -3,16 +3,24 @@
 %%% authors: Christian Himpe ( 0000-0003-2194-6754 )
 %%% license: BSD-2-Clause (2019)
 
+if(exist('OCTAVE_VERSION','builtin'))
+    svd_driver('gesvd');
+end
+
+RUNME
+
 emgrtest();
 
-moretests(1);
-moretests(0);
-moretests(Inf);
+moretests('i');
+moretests('s');
+moretests('c');
+moretests('r');
 
 examples('hnm');
 examples('isp');
 examples('fss');
 examples('nrc');
+examples('rqo');
 examples('lte');
 examples('fbc');
 examples('qso');
